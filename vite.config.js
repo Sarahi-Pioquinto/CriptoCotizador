5f06e8a991d3bc45ff8de2d919fa30d9f7397e3c
Sarah López <a3521110542@alumno.uttehuacan.edu.mx>
2022-11-09 21:49:55 -0600

    Subiendo el 9/11/2022

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { VitePWA } from 'vite-plugin-pwa'
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), VitePWA({
+    manifest: {
+      name: 'Cotizador de Criptomonedas con Vue, VITE y SASS',
+      short_name: 'CriptoCotizador',
+      description: 'Cotizador de Criptomonedas',
+      theme_color: '#f60de3',
+      icons: [
+        {
+          src: 'logo.svg',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+      ],
+    },
+  })],
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
